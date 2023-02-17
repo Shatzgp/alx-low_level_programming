f0db55794ead90afe1a6b760a75ced3ba9faaa44
phathutshedzo <phathumashathini@gmail.com>
2023-02-17 08:10:15 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the numbers from 00 to 99
+ * main -  program that prints all possible combinations of single-digit
  * Numbers must be separated by ,followed by a space
- * Numbers should be printed in ascending order with 2 digits
+ * Numbers should be printed in ascending order
  * You can only use the putchar function
  * Return: 0
  */
 
 int main(void)
 {
-int n1 = 48;
-int n2 = 48;
+int n = 48;
 int com = 44;
-while (n1 <= 57)
+while (n <= 57)
 {
-n2 = 48;
-while (n2 <= 57)
-{
-putchar(n1);
-putchar(n2);
-if (n1 != 57 || n2 != 57)
+putchar(n);
+if (n != 57)
 {
 putchar(com);
 putchar(32);
 }
-n2 += 1;
-}
-n1 += 1;
+n += 1;
 }
 putchar('\n');
 return (0);
