a59e2a6e6bcd8d3c99292a0b0338f255edde4e14
phathutshedzo <phathumashathini@gmail.com>
2023-02-17 08:07:32 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -  program that prints all possible combinations of single-digit
- * Numbers must be separated by ,followed by a space
- * Numbers should be printed in ascending order
+ * main -  program that prints all the numbers of base 16 in lowercase
  * You can only use the putchar function
  * Return: 0
  */
@@ -11,16 +9,16 @@
 int main(void)
 {
 int n = 48;
-int com = 44;
+int l = 'a';
 while (n <= 57)
 {
 putchar(n);
-if (n != 57)
-{
-putchar(com);
-putchar(32);
-}
 n += 1;
+}
+while (l <= 'f')
+{
+putchar(l);
+l++;
 }
 putchar('\n');
 return (0);
