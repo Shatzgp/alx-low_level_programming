81aba15d2231626390dad8bafce21fe3362aefd3
Shatzgp <phathumashathini@gmail.com>
2023-04-04 05:24:48 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *shatz;
+	listint_t *next;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{	
-		next = (*shatz)->next;
+		next = (*head)->next;
 		free(*head);
-		*head = shatz;
+		*head = next;
 	}
 
 	*head = NULL;
