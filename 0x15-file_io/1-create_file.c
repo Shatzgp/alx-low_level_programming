fead0cbbfb43e30bb0361227867e62014ba01df8
Shatzgp <phathumashathini@gmail.com>
2023-04-10 06:05:38 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: the name of the file to create.
- * @content: the content to write to the file.
+ * create_file - Create a new file or truncate an existing file with the given
+ * filename and write the given text content to it.
  *
- * Return: 1 on success, -1 on failure.
+ * @filename: The name of the file to create or truncate.
+ * @text_content: The text content to write to the file (may be NULL).
+ *
+ * Return: On success, returns 1. On failure, returns -1 and sets errno
+ * appropriately.
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int file_desc;
-	int num_chars;
-	int result;
+	int fd_new, w_new, len_new = 0;
 
-	if (!filename)
+	if (filename == NULL)
+	return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len_new = 0; text_content[len_new];)
+		len_new++;
+	}
+
+		fd_new = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		w_new = write(fd_new, text_content, len_new);
+
+		if (fd_new == -1 || w_new == -1)
 		return (-1);
 
-	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		close(fd_new);
 
-	if (file_desc == -1)
-		return (-1);
-
-	if (!content)
-		content = "";
-
-	for (num_chars = 0; content[num_chars]; num_chars++)
-		;
-
-	result = write(file_desc, content, num_chars);
-
-	if (result == -1)
-		return (-1);
-
-	close(file_desc);
-
-	return (1);
+			return (1);
 }
