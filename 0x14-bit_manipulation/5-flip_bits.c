1b6e744d66fe7481913d8d3484d81d6fd9f53290
Shatzgp <phathumashathini@gmail.com>
2023-04-09 18:53:57 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i;
 
-	for (i = 64; n || m; n >>= 1, m >>= 1)
+	for (i = 63; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			i++;
